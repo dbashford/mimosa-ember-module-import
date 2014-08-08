b0b2196360cf51feec71016e880c21f8046f5824
david.bashford <dbashford@bericotechnologies.com>
2014-08-07 23:14:57 -0400

    progress

--- src/config.js
+++ src/config.js
@@ -33,24 +33,22 @@ exports.defaults = function() {
 exports.placeholder = function() {
   var ph = "  emberResolver:                         # settings for ember-resolver module\n" +
            "    cacheDir:\".mimosa/emberResolver\"     # location to place cache. To not cache, set\n" +
-           "                                           # this to null. Path is relative to project root.\n" +
-           "    apps: [{                               # list of apps to create manifests for, one entry\n" +
-           "                                           # in the array for each app in your mimosa project\n" +
-           "      namespace: null,                     # the namespace of the app to create a manifest\n" +
-           "                                           # for. namespace = root folder. If namespace is null\n" +
-           "                                           # the entire project is used. namespace is relative\n" +
-           "                                           # to watch.javascriptDir.\n" +
-           "      additional: [],                      # Any additional/shared resources, either other\n" +
-           "                                           # namespaces or files to include in this manifest\n" +
-           "      exclude:[]                           # array of strings or regexes that match files\n" +
-           "                                           # to not include in this manifest. Strings are paths\n" +
-           "                                           # that can be relative to the manifest or absolute.\n" +
-           "                                           # If manifest is null, paths are relative to\n" +
-           "                                           # watch.javascriptDir.\n" +
+           "                                         # this to null. Path is relative to project root.\n" +
+           "    apps: [{                             # list of apps to create manifests for, one entry\n" +
+           "                                         # in the array for each app in your mimosa project\n" +
+           "      namespace: null,                   # the namespace of the app to create a manifest\n" +
+           "                                         # for. namespace = root folder. If namespace is null\n" +
+           "                                         # the entire project is used. namespace is relative\n" +
+           "                                         # to watch.javascriptDir.\n" +
+           "      additional: [],                    # Any additional/shared resources, either other\n" +
+           "                                         # namespaces or files to include in this manifest\n" +
+           "      exclude:[]                         # array of strings or regexes that match files\n" +
+           "                                         # to not include in this manifest. Strings are paths\n" +
+           "                                         # that can be relative to the manifest or absolute.\n" +
            "      manifestFile: \"app-modules\"        # The name of the manifest file to output.\n" +
-           "                                           # '.js' is assumed\n" +
-           "    }],                                    # \n" +
-           "    emberDirs: [                           # Ember directories that contain files to\n" +
+           "                                         # '.js' is assumed\n" +
+           "    }],                                  # \n" +
+           "    emberDirs: [                         # Ember directories that contain files to\n" +
            "      \"adapters\",                        # include in a manifest file. Any files in\n" +
            "      \"components\",                      # these directories or in subdirectories of\n" +
            "      \"controllers\",                     # these directories within a namespace will\n" +
@@ -82,20 +80,35 @@ exports.validate = function ( config, validators ) {
     if ( validators.isArrayOfObjects( errors, "emberResolver.apps", er.apps ) ) {
       er.apps.forEach( function( app ) {
         if ( validators.ifExistsIsString( errors, "emberResolver.apps.namespace", app.namespace ) ) {
-            if ( app.namespace ) {
-              app.namespace = path.join( config.watch.javascriptDir, app.namespace );
-            } else {
-              app.namespace = config.watch.javascriptDir;
-            }
+
+          // namespace can be null
+          if ( app.namespace ) {
+            app.namespace = path.join( config.watch.javascriptDir, app.namespace );
+          } else {
+            app.namespace = config.watch.javascriptDir;
+          }
+
+          if ( validators.ifExistsIsString( errors, "emberResolver.apps.manifestFile", app.manifestFile ) ) {
+            // manifestFile is relative to namespace
+            app.manifestFile = path.join( app.namespace, app.manifestFile + ".js" );
+          }
         }
-        validators.ifExistsIsString( errors, "emberResolver.apps.manifestFile", app.manifestFile );
-        validators.ifExistsIsArrayOfStrings(errors, "emberResolver.apps.additional", app.additional );
+
+        validators.ifExistsIsArrayOfStrings( errors, "emberResolver.apps.additional", app.additional );
         validators.ifExistsFileExcludeWithRegexAndString( errors, "emberResolver.apps.exclude", app, app.namespace );
       });
     }
 
     validators.isArrayOfStringsMustExist( errors, "emberResolver.emberDirs", er.emberDirs );
+  }
 
+  if ( !errors.length ) {
+    // populate each namespace with emberdirs for use of use later
+    er.apps.forEach( function( app ) {
+      app.emberDirs = er.emberDirs.map( function( emberDir ) {
+        return path.join( app.namespace, emberDir );
+      });
+    });
   }
 
   return errors;
