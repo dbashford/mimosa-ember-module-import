d07e60036c54608653dde55d4e88976aa58b1aab
david.bashford <dbashford@bericotechnologies.com>
2014-08-11 08:00:36 -0400

    testing updates

--- test/module_test.js
+++ test/module_test.js
@@ -6,33 +6,56 @@ var fs = require( 'fs' )
 var generateImportantPaths = function( projectName ) {
 
   var projectDirectory = path.join( __dirname, projectName );
+  var mimosaConfig = path.join( __dirname, projectName, "mimosa-config.js" );
   var publicDirectory = path.join( projectDirectory, "public" );
   var cacheDirectory = path.join( projectDirectory, ".mimosa" );
+  var cacheFile = path.join( projectDirectory, ".mimosa", "emberModuleImport", "cache.json" );
   var manifestFile = path.join( publicDirectory, "javascripts", "blogger", "modules.js");
+
 
   return {
     projectDir: projectDirectory,
     publicDir: publicDirectory,
     cacheDir: cacheDirectory,
-    manifest: manifestFile
+    cacheFile: cacheFile,
+    manifest: manifestFile,
+    mimosaConfig: mimosaConfig
   };
+}
+
+var writeConfig = function( configName, toProject ) {
+  var configInPath = path.join( __dirname, "configs", configName + ".js" );
+  var configOutPath = path.join( __dirname, toProject, "mimosa-config.js" );
+  var configText = fs.readFileSync( configInPath, "utf8" );
+  fs.writeFileSync(configOutPath, configText);
+}
+
+var cleanUp = function( paths ) {
+  // clean out cache
+  if ( fs.existsSync( paths.cacheDir ) ) {
+    wrench.rmdirSyncRecursive( paths.cacheDir );
+  }
+
+  // clean out public directory
+  if ( fs.existsSync( paths.publicDir ) ) {
+    wrench.rmdirSyncRecursive( paths.publicDir );
+  }
+
+  // remove mimosa config
+  if ( fs.existsSync( paths.mimosaConfig ) ) {
+    fs.unlinkSync( paths.mimosaConfig );
+  }
 }
 
 // fromscratch project
 describe('When starting from scratch with no cache', function() {
-  var paths = generateImportantPaths( "fromscratch" );
+  var paths = generateImportantPaths( "projectone" );
 
   this.timeout(15000);
 
   before(function(done){
-    if ( fs.existsSync( paths.cacheDir ) ) {
-      wrench.rmdirSyncRecursive( paths.cacheDir );
-    }
-
-    if ( fs.existsSync( paths.publicDir ) ) {
-      wrench.rmdirSyncRecursive( paths.publicDir );
-    }
-
+    cleanUp( paths );
+    writeConfig( "basic", "projectone" );
     var cwd = process.cwd();
     process.chdir( path.join( paths.projectDir ) );
     exec( "mimosa build", function ( err, sout, serr ) {
@@ -49,4 +72,12 @@ describe('When starting from scratch with no cache', function() {
     var text = fs.readFileSync( paths.manifest, "utf8" );
     expect(text).to.equal("require('./controllers/post_controller.js');\nrequire('./helpers/helpers.js');\nrequire('./routes/post_route.js');\nrequire('./routes/posts_route.js');\n");
   });
+
+  it( 'it will build the proper cache file', function() {
+    var cacheFileJSON = require( paths.cacheFile );
+    expect(Object.keys(cacheFileJSON).length).to.equal(1);
+    expect(cacheFileJSON[Object.keys(cacheFileJSON)[0]].length).to.equal(4);
+    expect(Object.keys(cacheFileJSON)[0]).to.equal(paths.manifest);
+  });
+
 });
